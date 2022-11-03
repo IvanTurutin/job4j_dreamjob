7dbd1b8f66903ef7898ebb8404ea4fa7db3857ed
Ivan <ivan.turutin@gmail.com>
2022-11-03 15:56:35 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -0,0 +1,73 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime create;
+
+    public Post() {
+    }
+
+    public Post(int id, String name, String description, LocalDateTime create) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.create = create;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreate() {
+        return create;
+    }
+
+    public void setCreate(LocalDateTime create) {
+        this.create = create;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id
+                && Objects.equals(name, post.name)
+                && Objects.equals(description, post.description)
+                && Objects.equals(create, post.create);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, create);
+    }
+}
