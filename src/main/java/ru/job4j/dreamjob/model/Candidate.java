c288bfaf07376293d6b053ac4278ebcc88c92996
Ivan <ivan.turutin@gmail.com>
2022-11-04 11:50:36 +0300

    5. Список кандидатов [#504842 #357554]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -60,14 +60,11 @@ public class Candidate {
             return false;
         }
         Candidate candidate = (Candidate) o;
-        return id == candidate.id
-                && Objects.equals(name, candidate.name)
-                && Objects.equals(description, candidate.description)
-                && Objects.equals(create, candidate.create);
+        return id == candidate.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, description, create);
+        return Objects.hash(id);
     }
 }
